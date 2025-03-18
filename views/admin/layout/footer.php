6ddffbb52bd7be9617993bc10656112f33104fe4
duythinh212004 <joker2102004@gmail.com>
2025-03-19 00:38:24 +0700

    thêm giao diện cho trang admin

--- views/admin/layout/footer.php
+++ views/admin/layout/footer.php
@@ -1,18 +1,31 @@
-<script src="admin/assets_admin/js/vendor.js"></script>
+</div>
+<!-- ./wrapper -->
 
-     <!-- App Javascript (Require in all Page) -->
-     <script src="admin/assets_admin/js/app.js"></script>
+<!-- REQUIRED SCRIPTS -->
+<!-- jQuery -->
+<script src="assets/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap -->
+<script src="assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- AdminLTE App -->
+<script src="assets/dist/js/adminlte.js"></script>
 
-     <!-- Vector Map Js -->
-     <script src="admin/assets_admin/vendor/jsvectormap/js/jsvectormap.min.js"></script>
-     <script src="admin/assets_admin/vendor/jsvectormap/maps/world-merc.js"></script>
-     <script src="admin/assets_admin/vendor/jsvectormap/maps/world.js"></script>
+<!-- PAGE PLUGINS -->
+<!-- jQuery Mapael -->
+<script src="assets/plugins/jquery-mousewheel/jquery.mousewheel.js"></script>
+<script src="assets/plugins/raphael/raphael.min.js"></script>
+<script src="assets/plugins/jquery-mapael/jquery.mapael.min.js"></script>
+<script src="assets/plugins/jquery-mapael/maps/usa_states.min.js"></script>
+<!-- ChartJS -->
+<script src="assets/plugins/chart.js/Chart.min.js"></script>
 
-     <!-- Dashboard Js -->
-     <script src="admin/assets_admin/js/pages/dashboard.js"></script>
-
+<!-- AdminLTE for demo purposes -->
+<script src="assets/dist/js/demo.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="assets/dist/js/pages/dashboard2.js"></script>
 </body>
+</html>
 
 
 <!-- Mirrored from techzaa.in/larkon/admin/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 23 Nov 2024 16:07:02 GMT -->
-</html>
